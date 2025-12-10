a1061c2a3fdbc19d07bcacabf426fe9f4e3cbb9d
Alps360 <h.alpsarioglu@gmail.com>
2025-12-10 12:52:05 +0300

    even counter

--- src/Lab6/EvenCounter.java
+++ src/Lab6/EvenCounter.java
@@ -2,8 +2,22 @@ package Lab6;
 
 public class EvenCounter {
     int countEven(int[] arr, int index) {
-        if(index == arr.length) 
+        // Base Case: Stop Condition
+        if (index == arr.length)
             return 0;
 
+        // Recursive Case : Subproblem handling
+        if (arr[index] % 2 == 0)
+            return 1 + countEven(arr, index + 1);
+        else
+            return countEven(arr, index + 1);
     }
+
+    public static void main(String[] args) {
+        EvenCounter ec = new EvenCounter();
+        int[] arr = {3, 6, 4, 7};
+        System.out.println("Count of even numbers: " + ec.countEven(arr, 0));
+    }
+
+
 }
